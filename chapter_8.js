9df1b8e9bc4a6ac3ca2e10022387b2360d23a496
Corey <mcpenny@yahoo.com>
2022-04-22 12:28:31 -0700

    Adding morr chapters

--- chapter_8.js
+++ chapter_8.js
@@ -1,4 +1,75 @@
 'use strict';
-test('toUpperCase', () => {
-	return 'hello'.toUpperCase === 'HELLO';
+
+// ## Multiplication ## //
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply (a, b){
+	if (Math.random() < 0.2) {
+		return a * b;
+	} else {
+		throw new MultiplicatorUnitFailure('Klunk');
+	}
+}
+
+function reliableMultiply (a, b){
+	for (;;) {
+		try {
+			let result = primitiveMultiply(a, b);
+			console.log(result);
+			break;
+		} catch (e) {
+			if (e instanceof MultiplicatorUnitFailure) {
+				console.log('Related Error');
+			} else {
+				throw e;
+			}
+		}
+	}
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
+
+// ## A locked box ## //
+
+const box = {
+	locked: true,
+	unlock () {
+		this.locked = false;
+	},
+	lock () {
+		this.locked = true;
+	},
+	_content: [],
+	get content () {
+		if (this.locked) throw new Error('Locked!');
+		return this._content;
+	}
+};
+
+function withBoxUnlocked (body){
+	if (box.locked == false) {
+		return box.content;
+	}
+	if (box.locked == true) {
+		box.unlock();
+		body();
+		console.log(box.content);
+	}
+	console.log('Closing the box....hehehehehe');
+	box.lock();
+}
+
+withBoxUnlocked(function (){
+	box.content.push('gold piece');
 });
+
+try {
+	withBoxUnlocked(function (){
+		throw new Error('Pirates on the horizon! Abort!');
+	});
+} catch (e) {
+	console.log('Error raised: ' + e);
+}
+console.log(box.locked);
+// → true
--- chapter_9.js
+++ chapter_9.js
@@ -1,1 +1,55 @@
-"use strict";
+'use strict';
+
+//## ##//
+// Fill in the regular expressions
+
+verify(/.../, [ 'my car', 'bad cats' ], [ 'camper', 'high art' ]);
+
+verify(/.../, [ 'pop culture', 'mad props' ], [ 'plop', 'prrrop' ]);
+
+verify(/.../, [ 'ferret', 'ferry', 'ferrari' ], [ 'ferrum', 'transfer A' ]);
+
+verify(/.../, [ 'how delicious', 'spacious room' ], [ 'ruinous', 'consciousness' ]);
+
+verify(/.../, [ 'bad punctuation .' ], [ 'escape the period' ]);
+
+verify(/.../, [ 'Siebentausenddreihundertzweiundzwanzig' ], [ 'no', 'three small words' ]);
+
+verify(/.../, [ 'red platypus', 'wobbling nest' ], [ 'earth bed', 'learning ape', 'BEET' ]);
+
+function verify (regexp, yes, no){
+	// Ignore unfinished exercises
+	if (regexp.source == '...') return;
+	for (let str of yes)
+		if (!regexp.test(str)) {
+			console.log(`Failure to match '${str}'`);
+		}
+	for (let str of no)
+		if (regexp.test(str)) {
+			console.log(`Unexpected match for '${str}'`);
+		}
+}
+
+//## ##//
+
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/A/g, 'B'));
+// → "I'm the cook," he said, "it's my job."
+
+//## ##//
+
+// Fill in this regular expression.
+let number = /^...$/;
+
+// Tests:
+for (let str of [ '1', '-1', '+15', '1.55', '.5', '5.', '1.3e2', '1E-4', '1e+12' ]) {
+	if (!number.test(str)) {
+		console.log(`Failed to match '${str}'`);
+	}
+}
+for (let str of [ '1a', '+-1', '1.2.3', '1+1', '1e4.5', '.5.', '1f5', '.' ]) {
+	if (number.test(str)) {
+		console.log(`Incorrectly accepted '${str}'`);
+	}
+}
