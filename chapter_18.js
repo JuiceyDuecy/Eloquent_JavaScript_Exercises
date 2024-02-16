00cae518b9e66a5e54b11925a3fddb528832339b
Corey <mcpenny@yahoo.com>
2024-02-15 21:26:00 -0800

    Adding work done to later chapters

--- chapter_18.js
+++ chapter_18.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+
+//HTTP and Forms
+
+console.log(encodeURIComponent('Yes?'));
+
+console.log(decodeURIComponent('Yes%3F'));
+
+fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0', {
+	headers: { Range: 'bytes=8-19' },
+	method: 'GET'
+})
+	.then((res) => res.text())
+	.then((text) => console.log(text));
+
+//Exercise 1
+
+fetch('https://eloquentjavascript.net/author', {
+	headers: { Accept: '*/*' },
+	method: 'GET'
+})
+	.then((res) => res.text())
+	.then((text) => console.log(text));
